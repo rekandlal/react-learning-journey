da320005729b35deda80972a3d0663ca98b6fd25
Abhishekh <rekandlal11@gmail.com>
2026-06-04 01:08:37 +0530

    Two Way Data Binding

--- Form Handling/src/App.jsx
+++ Form Handling/src/App.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
 
-  const submitHandler = (e) =>{
-    e.preventDefault()
-    console.log("Form Submitted")
-  } 
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
 
   return (
     <div>
-      <form onSubmit={(e) => {
-        submitHandler(e)
-      }}>
+      <form onSubmit={handleSubmit}>
         <div className="container">
           <input
             type="text"
             placeholder="Enter your name"
             className="input-box"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
 
-          <button className="submit-btn">
+          <button type="submit" className="submit-btn">
             Submit
           </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
